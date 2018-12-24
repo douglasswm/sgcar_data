916d5bb9603e3740f66e8cd198ea6d79bf15eb43
Douglas Sim <douglasswm@gmail.com>
2018-12-24 09:03:07 +0800

    feat: parse data to mongodb

--- main.py
+++ main.py
@@ -1,80 +1,32 @@
-#crawl sgcarmart car data into an array of objects
-from bs4 import BeautifulSoup
-import requests
+from requests import get
+from bs4 import BeautifulSoup as bs
 import json
-from pymongo import MongoClient
+from time import sleep
+from time import time
+from random import randint
+from IPython.core.display import clear_output
+from warnings import warn
 
-client = MongoClient('mongodb://localhost:27017/')
+url = 'https://www.sgcarmart.com/used_cars/listing.php?BRSR=1000&RPG=100'
+response = get(url)
 
-db = client.cars
-bmws = db.bmw
+html_soup = bs(response.text, 'html.parser')
+# print(html_soup)
 
-numberOfRecords = 0
+data = html_soup.select("#contentblank > table")[0].find_all('table',{"width":"100%"},cellpadding = 0,cellspacing = 0,border = 0, style=lambda value: value and 'table-layout: fixed' in value)
+# print(len(data))
+test = data[0].find('td', align="center", width= "82", valign= "top").find('font', {'color':'#009900'})
+print(test)
+# start_time = time()
+# requests = 0
 
-sgcarmart_url = 'https://www.sgcarmart.com/used_cars/listing.php?BRSR='+ str(numberOfRecords) +'&MOD=bmw&RPG=100&AVL=2'
-carro_url = 'https://carro.sg/buy-car?page=1&query=BMW&sortBy=carro_showroom_sort_newest'
+# for _ in range(5):
+#     # A request would go here
+#     requests += 1
+#     sleep(randint(1,3))
+#     current_time = time()
+#     elapsed_time = current_time - start_time
+#     print('Request: {}; Frequency: {} requests/s'.format(requests, requests/elapsed_time))
+#     clear_output(wait = True)
 
-page_response = requests.get(carro_url, timeout=5)
-
-data = BeautifulSoup(page_response.content, "html.parser")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# car_name = data.find_all('div', 
-# style=lambda value: value and 'width:186px' in value and 'padding-left:4px' in value)
-
-# car_price = data.find_all('div',
-# style=lambda value: value and 'width:67px' in value and 'font-weight:bold' in value)
-
-
-# def getData():
-#     list_car = []
-#     for name in car_name:
-#         print(name.find('a').text)
-#         car_name = name.find('a').text
-#         records = {
-#             'car_name' : car_name
-#         }
-#         list_car.append(records)
-
-#     print(len(list_car))
-#     bmws.insert_many(list_car)
-
-
-
-
-# print(list_car)
-# print(type(list_car))
-
-
-#Encode data to JSON
-# with open('data.json', 'w') as outfile:
-#     json.dump(list_car, outfile, indent=4)
-
-
-
-
-
-
-
-    
+# warn("Warning Simulation")    
--- sgcarmart.py
+++ sgcarmart.py
@@ -1,78 +1,98 @@
 from requests import get
 from bs4 import BeautifulSoup as bs
 import json
-BRSR = 0
+from time import sleep
+from time import time
+from random import randint
+from IPython.core.display import clear_output
+from warnings import warn
+from pymongo import MongoClient
+from pymongo.collection import ReturnDocument
 
-url = 'https://www.sgcarmart.com/used_cars/listing.php?BRSR=' + str(BRSR) + '&RPG=100'
+#Initialise DB
+client = MongoClient('mongodb://localhost:27017/')
+db = client.sgcarmart
+carsCollection = db.sgcarmart_cars
 
-# url = 'https://www.sgcarmart.com/used_cars/listing.php?RPG=20&ORD='
-response = get(url)
-# print(response.text[:500])
+# Preparing the monitoring of the loop
+start_time = time()
+requests = 0
+pages = [str(i) for i in range(0,100000,100)]
 
-html_soup = bs(response.text, 'html.parser')
-type(html_soup)
 
-data = html_soup.select("#contentblank > table")[0].find_all('table',{"width":"100%"},cellpadding = 0,cellspacing = 0,border = 0, style=lambda value: value and 'table-layout: fixed' in value)
-print(data)
-print(len(data))
+# For every page in the interval 100 records
+for page in pages:
 
-first_car = data[1]
+    # Make a get request
+    url = 'https://www.sgcarmart.com/used_cars/listing.php?BRSR=' + page + '&RPG=100&AVL=2'
 
-first_car_model = first_car.strong.a.text.strip()
-print('Model: '+first_car_model)
+    # Test for sold cars data
+    sold_cars_url = 'https://www.sgcarmart.com/used_cars/listing.php?BRSR=' + page + '&RPG=100&AVL=1'
 
-first_car_price = first_car.find('td', class_ ='font_red').div.text.strip()
-print('Price: '+first_car_price)
+    # Fetch page
+    response = get(url)
 
-first_car_depreciation = first_car.find('div', style=lambda value: value and 'width:101px' in value).text.strip()
-print('Depreciation: '+first_car_depreciation)
+    # Pause the loop
+    sleep(randint(6,12))
 
-first_car_reg_date = first_car.find('div', style=lambda value: value and 'width:89px' in value).text.strip()
-print('Reg Date: '+first_car_reg_date)
+    # Monitor the requests
+    requests += 1
+    elapsed_time = time() - start_time
+    print('Request:{}; Frequency: {} requests/s'.format(requests, requests/elapsed_time))
+    clear_output(wait = True)
 
-first_car_eng_cap = first_car.find('div', style=lambda value: value and 'width:84px' in value).text.strip()
-print('Eng Cap: '+first_car_eng_cap)
+    # Throw a warning for non-200 status codes
+    if response.status_code != 200:
+        warn('Request: {}; Status code: {}'.format(requests, response.status_code))
+    
+    html_soup = bs(response.text, 'html.parser')
 
-first_car_mileage = first_car.find('div', style=lambda value: value and 'width:83px' in value).text.strip()
-print('Mileage: '+first_car_mileage)
+    # Select all the car div from a single page
+    data = html_soup.select("#contentblank > table")[0].find_all('table',{"width":"100%"},cellpadding = 0,cellspacing = 0,border = 0, style=lambda value: value and 'table-layout: fixed' in value)
 
-first_car_veh_type = first_car.find('a', class_ = 'link_black nounderline').text.strip()
-print('Veh Type: '+first_car_veh_type)
+    # Break the loop if no records on page
+    if html_soup.find('td', width='175', height='160', valign='top') is not None:
+        warn('===============NO MORE RECORDS===============') 
+        print('===============NO MORE RECORDS===============') 
+        break 
+    
+    #for each car div
+    for car in data:
 
-first_car_status = first_car.find('td', align="center", width= "82", valign= "top").div.strong.text.strip()
-print('Status: '+ first_car_status)
+        #if price data is present, then:
+        if car.find('div', style=lambda value: value and 'width:67px' in value and 'font-weight:bold' in value) is not None and car.find('td', align="center", width= "82", valign= "top").find('font', {'color':'#009900'}) is not None:
+            
+            model = car.strong.a.text.strip()
+            price = car.find('div', style=lambda value: value and 'width:67px' in value and 'font-weight:bold' in value).text.strip()
+            depreciation = car.find('div', style=lambda value: value and 'width:101px' in value).text.strip()
+            reg_date = car.find('div', style=lambda value: value and 'width:89px' in value).text.strip()
+            eng_cap = car.find('div', style=lambda value: value and 'width:84px' in value).text.strip()
+            mileage = car.find('div', style=lambda value: value and 'width:83px' in value).text.strip()
+            veh_type = car.find('a', class_ = 'link_black nounderline').text.strip()
+            status = car.find('td', align="center", width= "82", valign= "top").div.strong.text.strip()
+            posted_date = car.find('td', class_ = 'font_gray_light font_10').text.strip()
 
-#Need to clean the data of Posted:
-first_car_ad_posted_date = first_car.find('td', class_ = 'font_gray_light font_10').text.strip()
-print(first_car_ad_posted_date)
-results = []
-for car in data:
+            car_data = {
+                'model' : model,
+                'price' : price,
+                'depreciation' : depreciation,
+                'registrationDate' : reg_date,
+                'engineCapacity' : eng_cap,
+                'mileage' : mileage,
+                'vehicleType' : veh_type,
+                'status' : status,
+                'postedDate' : posted_date
+            }
 
-    if car.find('div', style=lambda value: value and 'width:67px' in value and 'font-weight:bold' in value) is not None:
+            update = carsCollection.find_one_and_update(car_data, {"$set":car_data},upsert=True,return_document=ReturnDocument.BEFORE)
+            
+            if update == None:
+                print(update)
+                print("===A RECORD HAS BEEN INSERTED INTO DB===")
+            else:
+                print("=====RECORD ALREADY EXIST=====")    
         
-        model = car.strong.a.text.strip()
-        price = car.find('div', style=lambda value: value and 'width:67px' in value and 'font-weight:bold' in value).text.strip()
-        depreciation = car.find('div', style=lambda value: value and 'width:101px' in value).text.strip()
-        reg_date = car.find('div', style=lambda value: value and 'width:89px' in value).text.strip()
-        eng_cap = car.find('div', style=lambda value: value and 'width:84px' in value).text.strip()
-        mileage = car.find('div', style=lambda value: value and 'width:83px' in value).text.strip()
-        veh_type = car.find('a', class_ = 'link_black nounderline').text.strip()
-        status = car.find('td', align="center", width= "82", valign= "top").div.strong.text.strip()
-        # posted_date = car.find('td', class_ = 'font_gray_light font_10').text.strip()
 
-        car_data = {
-            'model' : model,
-            'price' : price,
-            'depreciation' : depreciation,
-            'registrationDate' : reg_date,
-            'engineCapacity' : eng_cap,
-            'mileage' : mileage,
-            'vehicleType' : veh_type,
-            'status' : status
-        }
-
-        results.append(car_data)
-
-#Encode data to JSON
-with open('data.json', 'w') as outfile:
-    json.dump(results, outfile, indent=4)
+    # print('Number Of Cars: '+ str(len(results)))
+    # carsCollection.insert_many(results)
+    # print('===' + str(len(results)) + ' RESULTS INSERTED INTO DB===')
